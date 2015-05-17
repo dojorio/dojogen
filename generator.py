4bccffad7c27323cd32d8689486f874a3b177d5e
Joao Felipe <joaofelipenp@gmail.com>
2015-05-17 13:07:56 -0300

    Using Argparser

--- generator.py
+++ generator.py
@@ -2,7 +2,10 @@ from datetime import datetime
 import os
 import sys
 import re
+import argparse
 
+
+TODAY = datetime.today().strftime("%Y%m%d")
 GENERATOR_MESSAGES = {
     'error': 'Generator failed to generate files',
     'message': "Generating Folder...",
@@ -11,81 +14,62 @@ GENERATOR_MESSAGES = {
     'exists': "Dojo path already exists!",
 }
 
+
 join = lambda a, b: os.path.join(os.path.abspath(a), b)
+
 
 def sprint(text, show):
     if show:
         print(text)
 
+
+def list_to_args(args, show=True):
+    opts = args.split(' ')
+    if len(opts) < 2:
+        raise GENERATOR_MESSAGES['error']
+    n = argparse.Namespace()
+    n.language, n.problem = opts[:2]
+    n.extra = opts[2:]
+    return n
+
+
 class Generator(object):
 
-    def __init__(self, args, messages = GENERATOR_MESSAGES):
-        self.opts = args.split(" ")
-        self.errors = []
+    def __init__(self, args):
+        self.language = args.language
+        self.problem = args.problem
+        self.extra =  ('_' + '_'.join(args.extra)) if args.extra else ''
 
-        self.language = ""
-        self.problem_name = ""
-        self.extra_name = ""
+        self.today = TODAY
 
-        self.today = datetime.today().strftime("%Y%m%d")
-
-        self.folder_name = ""
-        self.folder_path = ""
-        self.generator_path = ""
-        self.capitalized_name = ""
-
-        self.messages = messages
-
-
-    def show_errors(self, show):
-        for error in self.errors:
-            sprint(error, show)
-        return False
-
-    def parse_opts(self, show=True):
-        if len(self.opts) < 2:
-            self.errors.append(self.messages['error'])
-        self.show_errors(show)
-        return not bool(self.errors)
-
-    def read_opts(self, show=True):
-        if self.errors:
-            return self.show_errors(show)
-        self.language = self.opts.pop(0)
-        self.problem_name = self.opts.pop(0)
-        self.extra_name = "_".join(self.opts)
-        if self.extra_name:
-            self.extra_name = "_" + self.extra_name
-
-        self.folder_name = "%s%s_%s_%s" %(
-            self.today,
-            self.extra_name,
-            self.language,
-            self.problem_name
+        self.folder_name = "{date}{_extra}_{language}_{problem}".format(
+            date=TODAY,
+            _extra=self.extra,
+            language=self.language,
+            problem=self.problem,
         )
 
         self.folder_path = join(os.path.curdir, self.folder_name)
-        self.generator_path = join(os.path.dirname(__file__), "generators/%s/" %(self.language))
+        self.generator_path = join(
+            os.path.dirname(__file__), "generators/%s/" %(self.language))
 
-        splitted_name =  self.problem_name.split('_')
+        splitted_name =  self.problem.split('_')
 
-        snake_case = self.problem_name
+        snake_case = self.problem
         pascal_case = ''.join(part.capitalize() for part in splitted_name)
         down_case = ''.join(splitted_name)
-        camel_case = splitted_name.pop(0) + ''.join([part.capitalize() for part in splitted_name])
+        camel_case = (
+            splitted_name.pop(0) +
+            ''.join(part.capitalize() for part in splitted_name)
+        )
 
         self.cases = {
-
             '#_#dojotools#_#' : snake_case,
             '#_#class_dojotools#_#' : pascal_case,
             '#_#down_dojotools#_#' : down_case,
             '#_#camel_dojotools#_#' : camel_case,
-
-
         }
 
-
-        return True
 
     def replace(self, text):
         for sub, replace in self.cases.iteritems():
@@ -104,50 +88,52 @@ class Generator(object):
                 self.copy_and_rename(folder_path, infile, gen_path)
             else:
                 new_path = join(folder_path,self.replace(infile))
-                #with open(new_path, 'w') as w, open(gen_path, 'r') as r: #python 2.7
                 with open(new_path, 'w') as w:
                     with open(gen_path, 'r') as r:
                         for line in r:
                             w.write(self.replace(line))
 
     def generate(self, show=True):
-        if self.errors:
-            return self.show_errors(show)
         if not os.path.exists(self.folder_path):
-            sprint(self.messages['message'], show)
+            sprint(GENERATOR_MESSAGES['message'], show)
 
             if os.path.exists(self.generator_path):
-                sprint(self.messages['lang'] %(self.language), show)
-                self.copy_and_rename(os.path.curdir, self.folder_name, self.generator_path)
+                sprint(GENERATOR_MESSAGES['lang'] % (self.language), show)
+                self.copy_and_rename(
+                    os.path.curdir, self.folder_name, self.generator_path)
+                self.generated = True
             else:
-                self.errors.append(self.messages['lang_error'] %(self.language))
-                self.show_errors(show)
+                raise GENERATOR_MESSAGES['lang_error'] % (self.language)
         else:
-            self.errors.append(self.messages['exists'])
-            self.show_errors(show)
+            self.generated = True
+            raise GENERATOR_MESSAGES['exists']
 
-        return not bool(self.errors)
 
     def generated(self):
         return ((not self.errors) or self.messages['exists'] in self.errors)
 
 
-def generate(generate, directory, messages):
-    generator = Generator(generate, messages=messages)
-    if generate and generator.parse_opts():
-        generator.read_opts()
-        generator.generate()
-        if generator.generated() and directory == os.path.abspath(os.path.curdir):
-            return generator.folder_path
+def generate(generate, directory):
+    if generate:
+        generator = Generator(list_to_args(generate))
+        try:
+            generator.generate()
+        finally:
+            if generator.generated:
+                return generator.folder_path
     return directory
 
+
 if __name__ == '__main__':
-    args = sys.argv
-    args.pop(0)
-    generator = Generator(' '.join(args), messages=GENERATOR_MESSAGES)
-    if generator.parse_opts():
-        generator.read_opts()
-        generator.generate()
-
-
-
+    parser = argparse.ArgumentParser(
+        description='''Generates directory for coding dojo following the pattern:
+            {}[_extra]_language_problem\n'''.format(TODAY))
+    parser.add_argument('language', type=str,
+                        help='Programming Language')
+    parser.add_argument('problem', type=str,
+                        help='Problem Name')
+    parser.add_argument('extra', type=str, nargs='*', default='',
+                        help='Extra identifier')
+    args = parser.parse_args()
+    generator = Generator(args)
+    generator.generate()
